f0ce9849bf7c8328472b2b49f3635e066197665d
Devin Suy <54761385+devinsuy@users.noreply.github.com>
2023-12-07 15:32:44 -0800

    Update tableau compensation public src url

--- client/src/views/Home.tsx
+++ client/src/views/Home.tsx
@@ -2,12 +2,22 @@ import React, { useRef, useState } from 'react'
 import '../styles/views/Home.css'
 import { TableauEmbed, type TableauEmbedProps } from './TableauEmbed'
 import { NavHeader } from 'NavHeader'
-import { CloudCountryIndia, CloudCountryUS, CloudRemoteAllowedNo, CloudRemoteAllowedYes, CountryCounts, JobDesc, RemoteAllowedCounts, RemoteByCountry, RemoteByYear } from './remote/plots'
+import {
+  CloudCountryIndia,
+  CloudCountryUS,
+  CloudRemoteAllowedNo,
+  CloudRemoteAllowedYes,
+  CountryCounts,
+  JobDesc,
+  RemoteAllowedCounts,
+  RemoteByCountry,
+  RemoteByYear,
+} from './remote/plots'
 import { DropdownSelect } from './DropdownSelect'
 
 export const Home = () => {
   const compensationVizProps: TableauEmbedProps = {
-    workbookUrl: 'Compensation_17005461737460/Dashboard1',
+    workbookUrl: 'Compensation_17019148466490/Dashboard1',
     newDataUrl: '',
   }
 
@@ -17,7 +27,7 @@ export const Home = () => {
   }
   const jobTitleVizProps: TableauEmbedProps = {
     workbookUrl: 'PercantageoflistingsbyJobTitle/Dashboard1',
-    newDataUrl: ''
+    newDataUrl: '',
   }
 
   const compRef = useRef<HTMLDivElement>(null)
@@ -26,8 +36,19 @@ export const Home = () => {
   const titleRef = useRef<HTMLDivElement>(null)
 
   // Conditionally select which word cloud type to render
-  const options = ['All Job Listings', 'United States', 'India', 'Remote Allowed', 'Remote Not Allowed']
-  type WordCloudEnum = 'India' | 'United States' | 'All Job Listings' | 'Remote Allowed' | 'Remote Not Allowed'
+  const options = [
+    'All Job Listings',
+    'United States',
+    'India',
+    'Remote Allowed',
+    'Remote Not Allowed',
+  ]
+  type WordCloudEnum =
+    | 'India'
+    | 'United States'
+    | 'All Job Listings'
+    | 'Remote Allowed'
+    | 'Remote Not Allowed'
   const [cloudType, setCloudType] = useState<WordCloudEnum>('All Job Listings')
 
   const getWordCloud = (): React.ReactElement => {
@@ -54,8 +75,8 @@ export const Home = () => {
           <h1 style={{ fontWeight: 'bold' }}>Interested in data science?</h1>
           {/* eslint-disable react/no-unescaped-entities */}
           <p style={{ fontSize: '1.2em' }}>
-            We'll help you understand the industry and find the job
-            that's right for you.
+            We'll help you understand the industry and find the job that's right
+            for you.
           </p>
         </div>
 
@@ -68,7 +89,9 @@ export const Home = () => {
 
         <div ref={titleRef}>
           <div className='section-text'>
-            <h1 style={{ fontWeight: 'bold' }}>What kind of job title can you expect?</h1>
+            <h1 style={{ fontWeight: 'bold' }}>
+              What kind of job title can you expect?
+            </h1>
           </div>
           <TableauEmbed {...jobTitleVizProps} />
         </div>
@@ -82,38 +105,65 @@ export const Home = () => {
         </div>
 
         <div className='section-text'>
-          <h1 style={{ fontWeight: 'bold' }}>
-            What did we learn?
-          </h1>
+          <h1 style={{ fontWeight: 'bold' }}>What did we learn?</h1>
         </div>
 
         <div style={{ display: 'flex', justifyContent: 'space-between' }}>
           <div>
-            <p style={{ fontWeight: 'bold', fontSize: '1.2em', marginBottom: '20px' }}>Remote Work By Country</p>
+            <p
+              style={{
+                fontWeight: 'bold',
+                fontSize: '1.2em',
+                marginBottom: '20px',
+              }}
+            >
+              Remote Work By Country
+            </p>
             <RemoteByCountry />
           </div>
           <div>
-            <p style={{ fontWeight: 'bold', fontSize: '1.2em', marginBottom: '20px' }}>Remote Work By Year</p>
+            <p
+              style={{
+                fontWeight: 'bold',
+                fontSize: '1.2em',
+                marginBottom: '20px',
+              }}
+            >
+              Remote Work By Year
+            </p>
             <RemoteByYear />
           </div>
         </div>
 
         <div style={{ marginTop: '20px' }}>
-          <p style={{ fontWeight: 'bold', fontSize: '1.2em', marginBottom: '20px' }}>Words most commonly mentioned in job descriptions</p>
+          <p
+            style={{
+              fontWeight: 'bold',
+              fontSize: '1.2em',
+              marginBottom: '20px',
+            }}
+          >
+            Words most commonly mentioned in job descriptions
+          </p>
         </div>
         <div style={{ marginTop: '20px' }}>
           <div style={{ marginBottom: '20px' }}>
             <p style={{ fontSize: '1.2em' }}>Select a filter:</p>
           </div>
-          <DropdownSelect options={options} onChange={(selectedOption: string) => {
-            setCloudType(selectedOption as WordCloudEnum)
-          }} />
-          <div style={{ marginTop: '20px' }}>
-            {getWordCloud()}
-          </div>
+          <DropdownSelect
+            options={options}
+            onChange={(selectedOption: string) => {
+              setCloudType(selectedOption as WordCloudEnum)
+            }}
+          />
+          <div style={{ marginTop: '20px' }}>{getWordCloud()}</div>
         </div>
 
-        <div ref={compRef} style={{ marginTop: '40px' }} className='section-text'>
+        <div
+          ref={compRef}
+          style={{ marginTop: '40px' }}
+          className='section-text'
+        >
           <h1 style={{ fontWeight: 'bold' }}>
             What kind of compensation can you expect?
           </h1>
